720beb4f906076e10d5a4e478c105329e8d3e4cb
Josue Garces Jouvin <josuejouvin27@gmail.com>
2025-11-03 00:35:39 -0500

    Add card UI and image to login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom'
-import LoginPage from '@/pages/auth/login'
-import RegisterPage from '@/pages/auth/register'
-import App from '@/App'
-import { PATHS } from '@/constants/paths'
+import App from '@/App';
+import { PATHS } from '@/constants/paths';
+import RegisterPage from '@/pages/auth/register';
+import { createBrowserRouter } from 'react-router-dom';
+import Login from './pages/auth/login';
 
 export const router = createBrowserRouter([
-    {
-        path: PATHS.Home,
-        element: <App />,
-    },
-    {
-        path: PATHS.Login,
-        element: <LoginPage />,
-    },
-    {
-        path: PATHS.Register,
-        element: <RegisterPage />,
-    },
-])
+  {
+    path: PATHS.Home,
+    element: <App />,
+  },
+  {
+    path: PATHS.Login,
+    element: <Login />,
+  },
+  {
+    path: PATHS.Register,
+    element: <RegisterPage />,
+  },
+]);
