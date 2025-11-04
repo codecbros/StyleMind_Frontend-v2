a4f0ee7214c318400bd5c8a97a296bf8a67be68e
Josue Garces Jouvin <josuejouvin27@gmail.com>
2025-11-04 10:36:01 -0500

    Update code based on feedback

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -57,36 +57,4 @@ export const customInstance = <T>(
   return promise;
 };
 
-// Instancia específica para login que acepta data con email/password y los convierte a headers
-export const customInstanceWithBodyAsHeaders = <T>(
-  config: AxiosRequestConfig,
-  options?: AxiosRequestConfig & { data?: { email: string; password: string } }
-): CancelablePromise<T> => {
-  const abortController = new AbortController();
-
-  // Tomar email y password de options.data si existen
-  const credentials = options?.data;
-
-  const promise = AXIOS_INSTANCE({
-    ...config,
-    ...options,
-    headers: {
-      ...config.headers,
-      ...options?.headers,
-      ...(credentials && {
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    },
-    data: undefined, // No enviar data en el body
-    signal: abortController.signal,
-  }).then(({ data }) => data) as CancelablePromise<T>;
-
-  promise.cancel = () => {
-    abortController.abort();
-  };
-
-  return promise;
-};
-
 export default customInstance;
