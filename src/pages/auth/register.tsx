f0f18b005f83eeb3b2d0eefd2466c25339da2a70
Josue Garces Jouvin <josuejouvin27@gmail.com>
2025-12-02 17:45:40 -0500

    Add RegisterForm component and update register page
    Created a new RegisterForm component and integrated it into the register page. The register page layout was updated to include an image section and a styled card with descriptive text and the registration form.

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,7 +1,43 @@
+import authImage from '@/assets/images/authImg.jpg';
+import RegisterForm from '../../components/auth/RegisterForm';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../../components/ui/card';
+
 export default function RegisterPage() {
-    return (
-        <div>
-            <h1>Register Page</h1>
-        </div>
-    )
+  return (
+    <>
+      <section className="lg:col-span-1 2xl:col-span-3 mx-auto hidden lg:block ">
+        <img
+          src={authImage}
+          alt="Imagen de fondo para el registro"
+          width={3000}
+          height={2000}
+          className="object-cover transition-all duration-300 rounded-lg hover:grayscale opacity-90 dark:opacity-75"
+        />
+      </section>
+      <section className="mx-auto w-full lg:col-span-1 2xl:col-span-2">
+        <Card className="border border-muted-foreground">
+          <CardHeader>
+            <CardTitle className="font-extrabold text-center leading-7 md:text-start uppercase">
+              Descubre tu estilo único
+            </CardTitle>
+            <div className="space-y-1 pt-2">
+              <CardDescription className="text-sm text-muted-foreground">
+                Crea combinaciones personalizadas con IA y encuentra tu próximo
+                outfit perfecto
+              </CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <RegisterForm />
+          </CardContent>
+        </Card>
+      </section>
+    </>
+  );
 }
